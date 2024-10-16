4d08b26b32fe4dc4e739699ed80dc240ed7f45a1
Lama almassry <59393893+Lamaalmassry@users.noreply.github.com>
2024-10-16 12:14:30 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = var.AWS_DEFAULT_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+
 
 module "vpc" {
   source = "./modules/vpc"
