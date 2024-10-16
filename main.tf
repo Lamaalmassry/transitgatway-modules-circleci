e0a1699956367f3ab0137bc4439f198ed3f7e404
Lama almassry <59393893+Lamaalmassry@users.noreply.github.com>
2024-10-16 12:19:19 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = var.AWS_DEFAULT_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = getenv("AWS_DEFAULT_REGION")
+  access_key = getenv("AWS_ACCESS_KEY_ID")
+  secret_key = getenv("AWS_SECRET_ACCESS_KEY")
 }
 
 
