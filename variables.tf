b7097168379751402cf302be52ec42ddfc7db0c9
Lama almassry <59393893+Lamaalmassry@users.noreply.github.com>
2024-10-16 13:01:42 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,3 @@ variable "vpc_3_cidr" {}
 variable "vpc_1_private_cidr" {}
 variable "vpc_2_private_cidr" {}
 variable "vpc_3_public_cidr" {}
-variable "AWS_DEFAULT_REGION" {}
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
