93d9bc64ac2fe4dd48a85e42ffa12bdc1b826069
Lama almassry <59393893+Lamaalmassry@users.noreply.github.com>
2024-10-16 13:37:39 +0300

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -85,7 +85,7 @@ resource "aws_instance" "vpc_2_test" {
 
 resource "aws_key_pair" "key" {
   key_name   = var.key_name
-  public_key = file(var.public_key_path)
+  public_key =var.public_key_path
 
   tags = {
     Name = "my-key-pair"
